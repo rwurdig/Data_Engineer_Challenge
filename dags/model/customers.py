09c417a077f017ae0ca1ac60536248e34df377c3
Rodrigo Soares Wurdig <72509000+rwurdig@users.noreply.github.com>
2024-01-07 18:22:51 -0300

    Update customers.py

--- dags/model/customers.py
+++ dags/model/customers.py
@@ -1,1 +1,71 @@
+# Import required libraries
+import pandas as pd
+import numpy as np
+from sqlalchemy import inspect
+from logger.logger import logger
+from connection.postgres import connect_to_db
 
+
+# Initialize variables
+dataset_url = "https://media.githubusercontent.com/media/alfianhid/Building-a-Data-Warehousing-Pipeline-using-Python-Docker-Airflow-PostgreSQL-and-dbt/master/data/raw/customers_dataset.csv"
+columns = ['customer_id',
+           'customer_unique_id',
+           'customer_zip_code_prefix',
+           'customer_city',
+           'customer_state']
+
+
+# Define ETL methods
+@logger
+def extract_dataset(dataset_url):
+    print(f"Reading dataset from {dataset_url}...")
+    df = pd.read_csv(dataset_url, names=columns)
+    return df
+
+@logger
+def check_table_availability(table_name, engine):
+    if table_name in inspect(engine).get_table_names():
+        print(f"{table_name!r} already exists in database!")
+    else:
+        print(f"{table_name} does not exist in database!")
+
+@logger
+def load_to_db(df, table_name, engine):
+    print(f"Loading dataset to table: {table_name}...")
+    df.to_sql(table_name, engine, if_exists="replace")
+    check_table_availability(table_name, engine)
+
+@logger
+def clean_dataset(df):
+    print("Cleaning dataset...")
+    df = df.replace(r'^\s+$', np.nan, regex=True)
+    df = df.drop_duplicates()
+    df['customer_city'] = df['customer_city'].str.title()
+    df['customer_state'] = df['customer_state'].str.upper()
+    return df
+
+@logger
+def save_clean_dataset(df):
+    df.to_csv('data/clean/customers_dataset.csv')
+
+@logger
+def is_tables_exists():
+    db_engine = connect_to_db()
+    print("Checking if tables already exists...")
+    check_table_availability("raw_customers", db_engine)
+    check_table_availability("clean_customers", db_engine)
+
+@logger
+def customers_etl():
+    db_engine = connect_to_db()
+
+    raw_df = extract_dataset(dataset_url)
+    raw_table_name = "raw_customers"
+
+    clean_df = clean_dataset(raw_df)
+    clean_table_name = "clean_customers"
+    save_clean_dataset(clean_df)
+
+    load_to_db(raw_df, raw_table_name, db_engine)
+    load_to_db(clean_df, clean_table_name, db_engine)
+    is_tables_exists()
