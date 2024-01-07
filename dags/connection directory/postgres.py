b9882d6779ca92e988d48cd02c61f1c9c3ae85b3
Rodrigo Soares Wurdig <72509000+rwurdig@users.noreply.github.com>
2024-01-07 18:19:53 -0300

    Update postgres.py

--- dags/connection directory/postgres.py
+++ dags/connection directory/postgres.py
@@ -1,1 +1,32 @@
+import os
+from logger.logger import logger
+# from dotenv import dotenv_values
+from sqlalchemy import create_engine
 
+
+# Set environment config
+# CONFIG = dotenv_values(".env")
+# if not CONFIG:
+#     CONFIG = os.environ
+    
+
+# Create PostgreSQL connection
+@logger
+def connect_to_db():
+    print("Connecting to database...")
+    connection_uri = "postgresql+psycopg2://{}:{}@{}:{}/{}".format(
+        # CONFIG["POSTGRES_USER"],
+        # CONFIG["POSTGRES_PASSWORD"],
+        # CONFIG["POSTGRES_HOST"],
+        # CONFIG["POSTGRES_PORT"],
+        # CONFIG["POSTGRES_DB"],
+        os.getenv("POSTGRES_USER"),
+        os.getenv("POSTGRES_PASSWORD"),
+        os.getenv("POSTGRES_HOST"),
+        os.getenv("POSTGRES_PORT"),
+        os.getenv("POSTGRES_DB"),
+    )
+
+    engine = create_engine(connection_uri, pool_pre_ping=True)
+    engine.connect()
+    return engine
